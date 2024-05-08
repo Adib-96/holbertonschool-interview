72b955447b7c4cf358e4bf263b74cbc8ef45f0cf
Adib-96 <adibbh1996@gmail.com>
2024-05-08 10:01:03 +0100

    code documented

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,7 +1,7 @@
+#!/usr/bin/python3
 def canUnlockAll(boxes):
-    # Initial set of unlocked boxes starting with the first box
+    '''Function to inlock boxes'''
     unlocked = set([0])
-    # Set of keys collected from the boxes
     keys = set(boxes[0])
 
     # Keep checking until no new keys are added
@@ -9,7 +9,6 @@ def canUnlockAll(boxes):
         # Get a key to check
         current_key = keys.pop()
 
-        # If the key is for a box that hasn't been unlocked
         if current_key < len(boxes) and current_key not in unlocked:
             # Unlock the box
             unlocked.add(current_key)
@@ -18,13 +17,3 @@ def canUnlockAll(boxes):
 
     # If we've unlocked all the boxes
     return len(unlocked) == len(boxes)
-
-# Test the function with sample data
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))  # Output: True
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
