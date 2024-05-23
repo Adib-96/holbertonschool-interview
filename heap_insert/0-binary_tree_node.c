1315e7f318ff2386daa95ffb820ec498f79aae78
Adib-96 <adibbh1996@gmail.com>
2024-05-23 20:24:15 +0100

    fixing betty

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *newNode = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 if (newNode == NULL)
 {
-return NULL;
+return (NULL);
 }
 newNode->n = value;
 newNode->parent = parent;
 newNode->left = NULL;
 newNode->right = NULL;
-return newNode;
+return (newNode);
 }
