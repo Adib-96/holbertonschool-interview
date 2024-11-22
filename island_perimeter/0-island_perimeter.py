c650aeb98115c5ed15849ec4c74ef756d4357371
Adib-96 <adibbh1996@gmail.com>
2024-11-22 09:41:41 +0100

    refactor code

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
+"""
+Module to calculate the perimeter of an island in a grid.
+"""
 
-""" Function to find perimiter of an island """
 
 def island_perimeter(grid):
     """
-    Input: List of Lists
-    Returns: Perimeter of the island
+    Calculate the perimeter of the island described in the grid.
+
+    Args:
+        grid (list of list of ints)
+
+    Returns:
+        int: The perimeter of the island.
     """
     perimeter = 0
     rows = len(grid)
-    cols = len(grid[0]) if rows > 0 else 0
+    cols = len(grid[0])
 
-    for line in range(rows):
-        for cell in range(cols):
-            if grid[line][cell] == 1:
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Add 4 sides for every land cell
                 perimeter += 4
+                # Subtract 2 for each adjacent land cell (vertical and
+                # horizontal)
+                if i > 0 and grid[i - 1][j] == 1:  # Check above
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:  # Check left
+                    perimeter -= 2
 
-                if line > 0 and grid[line - 1][cell] == 1:
-                    perimeter -= 1
-                if cell > 0 and grid[line][cell - 1] == 1:
-                    perimeter -= 1
-                if cell < cols - 1 and grid[line][cell + 1] == 1:
-                    perimeter -= 1
-                if line < rows - 1 and grid[line + 1][cell] == 1:
-                    perimeter -= 1
     return perimeter
