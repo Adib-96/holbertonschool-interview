d6ad896d3856fdc9dadb62f8d2858989bc9aa7b5
Adib-96 <adibbh1996@gmail.com>
2024-11-29 07:12:10 +0100

    wild compare

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -14,10 +14,14 @@ return (*s1 == '\0');
 
 if (*s2 == '*')
 {
-while (*(s2 + 1) == '*')
-s2++;
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
 
-return (wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2)));
+if (wildcmp(s1, s2 + 1))
+return (1);
+
+if (*s1 && wildcmp(s1 + 1, s2))
+return (1);
 }
 
 if (*s1 == *s2)
